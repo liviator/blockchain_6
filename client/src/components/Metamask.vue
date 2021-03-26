7aa0e606b1136e89bc9bfd15810d22305065f54e
Henry Faure-Geors <h.f-g@hotmail.fr>
2021-03-26 11:59:01 +0100

    first commit

---
+++ client/src/components/Metamask.vue
@@ -0,0 +1,28 @@
+<template>
+  
+</template>
+
+<script>
+import VueMetamask from 'vue-metamask';
+
+export default {
+    components: {
+        VueMetamask,
+    },
+    data(){
+        return {
+            msg:"test"
+        }
+    },
+    methods: {
+        onComplete(data){
+            console.log('data:',data)
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
